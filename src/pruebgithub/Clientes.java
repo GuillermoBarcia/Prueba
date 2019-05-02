81978907ba91e1ad7a421b9d9c72f72b4f9c31bd
Admin <Admin@10.5.26.153>
2019-05-02 08:19:52 -0500

    Clree Una Clase

--- src/pruebgithub/Clientes.java
+++ src/pruebgithub/Clientes.java
@@ -3,12 +3,12 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package Clase;
+package pruebgithub;
 
 /**
  *
  * @author Admin
  */
 public class Clientes {
-    ///hjhhjhj
+    
 }
